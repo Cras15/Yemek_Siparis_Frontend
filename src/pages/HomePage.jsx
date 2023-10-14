c27ec5013e9b4a172106243c8660918503572cd8
mert <mertyener.my444@gmail.com>
2023-10-15 01:01:50 +0300

    cors fix test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,9 +4,28 @@ import React from 'react'
 
 const HomePage = () => {
   const fetchData = async () => {
-    await axios.get('/api/manager/shop/getMyShop').then(function (res) {
-      console.log(res.data);
-    })
+    let data = JSON.stringify({
+      "username": "asda@asd.asd",
+      "password": "123"
+    });
+
+    let config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'https://api.ayagimagelsin.com.tr/auth/login',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: data
+    };
+
+    await axios.request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
   return (
     <div>
