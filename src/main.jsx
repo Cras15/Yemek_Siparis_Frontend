b88c791fda299deea21b2a2f98571620608cba96
mert <mertyener.my444@gmail.com>
2023-10-25 10:57:42 +0300

    add persist storage

--- src/main.jsx
+++ src/main.jsx
@@ -2,26 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { store } from './redux/store.jsx'
+import store, { Persistor } from './redux/store.jsx'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import axios from 'axios'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { PersistGate } from 'redux-persist/integration/react'
 
 //axios.defaults.baseURL = 'https://api.ayagimagelsin.com.tr';
 /*if (process.env.NODE_ENV !== 'production')
   axios.defaults.baseURL = 'http://localhost:8080';
 else*/
-  axios.defaults.baseURL = 'https://api.ayagimagelsin.com.tr';
+axios.defaults.baseURL = 'https://api.ayagimagelsin.com.tr';
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Provider store={store}>
-      <GoogleOAuthProvider clientId='711966364839-742q3cumeeg14lalepnlsavb1cup74i6.apps.googleusercontent.com'>
-        <App />
-      </GoogleOAuthProvider>
+      <PersistGate loading={null} persistor={Persistor}>
+        <GoogleOAuthProvider clientId='711966364839-742q3cumeeg14lalepnlsavb1cup74i6.apps.googleusercontent.com'>
+          <App />
+        </GoogleOAuthProvider>
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 )
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,14 +1,30 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from './userSlice';
 import basketSlice from './basketSlice';
+import storage from 'redux-persist/lib/storage'
+import persistStore from 'redux-persist/es/persistStore';
+import { persistReducer } from 'redux-persist';
 
-export const store = configureStore({
+const persistConfig  = {
+    key: "user",
+    storage,
+}
+
+const userReducer = persistReducer(persistConfig, userSlice);
+const basketReducer = persistReducer({key: "basket", storage}, basketSlice);
+
+const store = configureStore({
     reducer: {
-        user: userSlice,
-        basket: basketSlice,
+        user: userReducer,
+        basket: basketReducer,
     },
     middleware: getDefaultMiddleware =>
         getDefaultMiddleware({
             serializableCheck: false,
         }),
 });
+
+const Persistor = persistStore(store);
+
+export { Persistor };
+export default store;
